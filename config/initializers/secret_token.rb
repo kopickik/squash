83660b0cc6b7bd95deaa65c66687e1d53a6502ac
Kyle Kopicki <kyle.kopicki@gmail.com>
2011-06-17 02:00:45 -0400

    initial commit this time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squash::Application.config.secret_token = 'f2f8102177b2949a8293db3f19bc780e3779c816c704bd963d9c375729993a53c7e1f96920e839fd7ed9fe086ebd916ce2b8e2bf659919ac9196063412d1c808'
