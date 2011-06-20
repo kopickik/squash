9fde730aa813bab8c380b0d54fb7c8f12cd91956
Kyle Kopicki <kyle.kopicki@gmail.com>
2011-06-20 14:10:31 -0400

    Removed default public index Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Squash::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'  
+  
   get "pages/home"
 
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +57,7 @@ Squash::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
